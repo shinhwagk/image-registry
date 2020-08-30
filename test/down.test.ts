a0ed42fff969113cf972f91564de53ffcddaa365
shinhwagk <shanghai_sl@hotmail.com>
2020-08-30 17:24:12 +0800

    test

--- test/down.test.ts
+++ test/down.test.ts
@@ -1,3 +1,3 @@
 import * as down from '../src/down';
 
-down.DownManager.create('https://quay.io/v2/openshift/okd-content/blobs/sha256:89eaaaf386250faa931481c7a091b8540c35739569482aaebe214e0c69999e7c', 'test', 'openshift/okd-content', 'blobs', '89eaaaf386250faa931481c7a091b8540c35739569482aaebe214e0c69999e7c').start()
+down.DownManager.create('https://quay.io/v2/openshift/okd-content/blobs/sha256:89eaaaf386250faa931481c7a091b8540c35739569482aaebe214e0c69999e7c', 'storage', 'openshift/okd-content', 'blobs', '89eaaaf386250faa931481c7a091b8540c35739569482aaebe214e0c69999e7c').start()
