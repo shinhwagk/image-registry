c7259215e1acb09b977d69af83f84a85d5a64fee
gk <shanghai_sl@hotmail.com>
2020-09-05 14:40:08 +0800

    0.2.6

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,18 @@ export const logLevel = process.env['level'] || 'info';
 
 // fix immutable
 export const chunkSize: string = process.env['chunksize'] || '1048576'; // default 1m.
+
+const proxyHost = process.env['host']
+const proxyPort = process.env['port']
+
+import * as tunnel from 'tunnel'
+
+function makeProxy(host: string, port: number) {
+    return {
+        https: tunnel.httpsOverHttp({
+            proxy: { host, port }
+        })
+    }
+}
+
+export const agent = proxyHost && proxyPort ? makeProxy(proxyHost, Number(proxyPort)) : {}
